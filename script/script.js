374407b05e56f7ecb6dfee24c43507127aea11c1
jeremy koundour <j.koundour@gmail.com>
2022-10-31 14:07:44 +0100

    mise en place du linter Eslint

--- script/script.js
+++ script/script.js
@@ -1,45 +1,45 @@
-import recipes from './recipes.js'
-import displayCardRecipes from './card.js'
-import search from './functions.js'
-import taglist, { displayTags } from './taglist.js'
+import recipes from './recipes.js';
+import displayCardRecipes from './card.js';
+import search from './functions.js';
+import taglist, { displayTags } from './taglist.js';
 
 taglist(recipes);
 
-let data = {
-    recipes : [...recipes],
-    mainSearch : '',
-    searchLength : 0,
-}
+const data = {
+  recipes: [...recipes],
+  mainSearch: '',
+  searchLength: 0,
+};
 
 const handler = {
-    set(obj, prop, value) {
-        obj[prop] = value;
-        switch(prop) {
-            case 'filtredRecipes':
-                displayCardRecipes(value);
-                displayTags(value)
-                break;
-            case 'mainSearch':
-            if(value.length > 2 && data.searchLength <= value.length){
-                proxy.filtredRecipes = search(data.filtredRecipes, value);
-            }else if(value.length > 2 && data.searchLength > value.length){
-                proxy.filtredRecipes = search(data.recipes, value);
-            }else{
-                proxy.filtredRecipes = [...data.recipes];
-            }
-            break
-            default:
-                break;
+  set(obj, prop, value) {
+    obj[prop] = value;
+    switch (prop) {
+      case 'filtredRecipes':
+        displayCardRecipes(value);
+        displayTags(value);
+        break;
+      case 'mainSearch':
+        if (value.length > 2 && data.searchLength <= value.length) {
+          proxy.filtredRecipes = search(data.filtredRecipes, value);
+        } else if (value.length > 2 && data.searchLength > value.length) {
+          proxy.filtredRecipes = search(data.recipes, value);
+        } else {
+          proxy.filtredRecipes = [...data.recipes];
         }
-        return true;
+        break;
+      default:
+        break;
     }
-}
+    return true;
+  },
+};
 
 let proxy = new Proxy(data, handler);
 
 proxy.filtredRecipes = [...recipes];
 
 document.querySelector('#searchRecipesInput').addEventListener('keyup', (e) => {
-    proxy.mainSearch = e.target.value
-    proxy.searchLength = e.target.value.length;
-})
+  proxy.mainSearch = e.target.value;
+  proxy.searchLength = e.target.value.length;
+});
--- script/taglist.js
+++ script/taglist.js
@@ -1,4 +1,4 @@
-export default function taglist(recipes){
+export default function taglist(recipes) {
 /*     // INGREDIENTS
     const allIngredients = [];
     for (let i=0; i<recipes.length; i++) {
@@ -12,11 +12,11 @@ export default function taglist(recipes){
     const ingredientNorepeat = new Set(allIngredients);
     console.log("ingredient no repeat", ingredientNorepeat);
 
- */    // OUVERTURE TAG INGREDIENT
-    document.querySelector('#btnIngredients').addEventListener('click', (e) => {
-        document.querySelector('#btnIngredients').style.setProperty('display', 'none');
-        document.querySelector('#taglistIngredients').style.setProperty('display', 'block');
-/*         const zoneList = document.createElement('ul');
+ */ // OUVERTURE TAG INGREDIENT
+  document.querySelector('#btnIngredients').addEventListener('click', (e) => {
+    document.querySelector('#btnIngredients').style.setProperty('display', 'none');
+    document.querySelector('#taglistIngredients').style.setProperty('display', 'block');
+    /*         const zoneList = document.createElement('ul');
         zoneList.setAttribute('class', 'zoneIngredientTag');
         document.querySelector('#listIngredient').appendChild(zoneList);
         for (const element of ingredientNorepeat) {
@@ -25,111 +25,112 @@ export default function taglist(recipes){
             list.innerText = element;
             console.log(element);
         };
- */        
-        document.querySelector('#btnUstensiles').style.setProperty('display', 'block');
-        document.querySelector('#taglistUstensiles').style.setProperty('display', 'none');
-        document.querySelector('#btnAppareils').style.setProperty('display', 'block');
-        document.querySelector('#taglistAppareils').style.setProperty('display', 'none');
+ */
+    document.querySelector('#btnUstensiles').style.setProperty('display', 'block');
+    document.querySelector('#taglistUstensiles').style.setProperty('display', 'none');
+    document.querySelector('#btnAppareils').style.setProperty('display', 'block');
+    document.querySelector('#taglistAppareils').style.setProperty('display', 'none');
+  });
+  // FERMETURE TAG INGREDIENT
+  document.querySelector('#iconTagIngredient').addEventListener('click', (e) => {
+    document.querySelector('#btnIngredients').style.setProperty('display', 'block');
+    document.querySelector('#taglistIngredients').style.setProperty('display', 'none');
+  });
 
-    });
-    // FERMETURE TAG INGREDIENT
-    document.querySelector('#iconTagIngredient').addEventListener('click', (e) => {
-        document.querySelector('#btnIngredients').style.setProperty('display', 'block');
-        document.querySelector('#taglistIngredients').style.setProperty('display', 'none');
-    });
+  // APPAREILS
+  const allAppareils = [];
+  for (let i = 0; i < recipes.length; i++) {
+    const appareils = recipes[i].appliance;
+    allAppareils.push(appareils);
+  }
 
-    // APPAREILS
-    const allAppareils = [];
-    for (let i=0; i<recipes.length; i++) {
-        let appareils = recipes[i].appliance;
-            allAppareils.push(appareils);
-    };
+  const appareilNorepeat = new Set(allAppareils);
 
-    const appareilNorepeat = new Set(allAppareils);
+  // OUVERTURE TAG APPAREILS
+  document.querySelector('#btnAppareils').addEventListener('click', (e) => {
+    document.querySelector('#btnAppareils').style.setProperty('display', 'none');
+    document.querySelector('#taglistAppareils').style.setProperty('display', 'block');
+    for (const element of appareilNorepeat) {
+      const list = document.createElement('p');
+      document.querySelector('#listAppareils').appendChild(list);
+      list.innerText = element;
+    }
+    document.querySelector('#btnIngredients').style.setProperty('display', 'block');
+    document.querySelector('#taglistIngredients').style.setProperty('display', 'none');
+    document.querySelector('#btnUstensiles').style.setProperty('display', 'block');
+    document.querySelector('#taglistUstensiles').style.setProperty('display', 'none');
+  });
+  // FERMETURE TAG APPAREILS
+  document.querySelector('#iconTagAppareils').addEventListener('click', (e) => {
+    document.querySelector('#btnAppareils').style.setProperty('display', 'block');
+    document.querySelector('#taglistAppareils').style.setProperty('display', 'none');
+  });
 
-    // OUVERTURE TAG APPAREILS
-    document.querySelector('#btnAppareils').addEventListener('click', (e) => {
-        document.querySelector('#btnAppareils').style.setProperty('display', 'none');
-        document.querySelector('#taglistAppareils').style.setProperty('display', 'block');
-        for (const element of appareilNorepeat) {
-            const list = document.createElement('p');
-            document.querySelector('#listAppareils').appendChild(list);
-            list.innerText = element;
-        };
-        document.querySelector('#btnIngredients').style.setProperty('display', 'block');
-        document.querySelector('#taglistIngredients').style.setProperty('display', 'none');
-        document.querySelector('#btnUstensiles').style.setProperty('display', 'block');
-        document.querySelector('#taglistUstensiles').style.setProperty('display', 'none');
-    });
-    // FERMETURE TAG APPAREILS
-    document.querySelector('#iconTagAppareils').addEventListener('click', (e) => {
-        document.querySelector('#btnAppareils').style.setProperty('display', 'block');
-        document.querySelector('#taglistAppareils').style.setProperty('display', 'none');
-    });
+  // USTENSILES
+  const allUstensiles = [];
+  for (let i = 0; i < recipes.length; i++) {
+    const ustensiles = recipes[i].ustensils;
+    allUstensiles.push(ustensiles);
+  }
 
+  const ustensileNorepeat = new Set(allUstensiles);
 
-    // USTENSILES
-    const allUstensiles = [];
-    for (let i=0; i<recipes.length; i++) {
-        let ustensiles = recipes[i].ustensils;
-        allUstensiles.push(ustensiles);
-    };
+  // OUVERTURE TAG USTENSILES
+  document.querySelector('#btnUstensiles').addEventListener('click', (e) => {
+    document.querySelector('#btnUstensiles').style.setProperty('display', 'none');
+    document.querySelector('#taglistUstensiles').style.setProperty('display', 'block');
+    for (const element of ustensileNorepeat) {
+      const list = document.createElement('p');
+      document.querySelector('#listUstensiles').appendChild(list);
+      list.innerText = element;
+    }
+    document.querySelector('#btnIngredients').style.setProperty('display', 'block');
+    document.querySelector('#taglistIngredients').style.setProperty('display', 'none');
+    document.querySelector('#btnAppareils').style.setProperty('display', 'block');
+    document.querySelector('#taglistAppareils').style.setProperty('display', 'none');
+  });
+  // FERMETURE TAG USTENSILES
+  document.querySelector('#iconTagUstensiles').addEventListener('click', (e) => {
+    document.querySelector('#btnUstensiles').style.setProperty('display', 'block');
+    document.querySelector('#taglistUstensiles').style.setProperty('display', 'none');
+  });
 
-    const ustensileNorepeat = new Set(allUstensiles);
-
-    // OUVERTURE TAG USTENSILES
-    document.querySelector('#btnUstensiles').addEventListener('click', (e) => {
-        document.querySelector('#btnUstensiles').style.setProperty('display', 'none');
-        document.querySelector('#taglistUstensiles').style.setProperty('display', 'block');
-        for (const element of ustensileNorepeat) {
-            const list = document.createElement('p');
-            document.querySelector('#listUstensiles').appendChild(list);
-            list.innerText = element;
-        };
-        document.querySelector('#btnIngredients').style.setProperty('display', 'block');
-        document.querySelector('#taglistIngredients').style.setProperty('display', 'none');
-        document.querySelector('#btnAppareils').style.setProperty('display', 'block');
-        document.querySelector('#taglistAppareils').style.setProperty('display', 'none');
-
-    });
-    // FERMETURE TAG USTENSILES
-    document.querySelector('#iconTagUstensiles').addEventListener('click', (e) => {
-        document.querySelector('#btnUstensiles').style.setProperty('display', 'block');
-        document.querySelector('#taglistUstensiles').style.setProperty('display', 'none');
-    });
-
-
- return appareilNorepeat, ustensileNorepeat;
-};
-
-export function displayTags(recipes){
-    const ingredients = getIngredientsFrom(recipes);
-    displayIngredients(ingredients);
+  return appareilNorepeat, ustensileNorepeat;
+}
+// MISE EN PLACE DES LISTES INGREDIENTS/APPAREILS/USTENSILES
+export function displayTags(recipes) {
+  const ingredients = getIngredientsFrom(recipes);
+  displayIngredients(ingredients);
 }
 
-function getIngredientsFrom(recipes){
-    const allIngredients = [];
-    for (let i=0; i<recipes.length; i++) {
-        let ingredients = recipes[i].ingredients;
-        ingredients.map(({ingredient}) => {
-            allIngredients.push(`${ingredient.toLowerCase()}`);
-        });
-    };
-    console.log("ingredient array", [allIngredients]);
-    const ingredientNorepeat = new Set(allIngredients);
-    console.log("ingredient no repeat", ingredientNorepeat);
-    return ingredientNorepeat;
+// INGREDIENTS
+function getIngredientsFrom(recipes) {
+  const allIngredients = [];
+  for (let i = 0; i < recipes.length; i++) {
+    const { ingredients } = recipes[i];
+    ingredients.map(({ ingredient }) => {
+      allIngredients.push(`${ingredient.toLowerCase()}`);
+    });
+  }
+  console.log('ingredient array', [allIngredients]);
+  const ingredientNorepeat = new Set(allIngredients);
+  console.log('ingredient no repeat', ingredientNorepeat);
+  return ingredientNorepeat;
 }
 
 function displayIngredients(ingredients) {
-    document.querySelector('#listIngredient').innerHTML = '';
-    const zoneList = document.createElement('ul');
-    document.querySelector('#taglistIngredients')
-    for (const element of ingredients) {
-        const list = document.createElement('li');
-        zoneList.appendChild(list);
-        list.innerText = element;
-        console.log(element);
-    };
-    document.querySelector('#listIngredient').appendChild(zoneList);
+  document.querySelector('#listIngredient').innerHTML = '';
+  const zoneList = document.createElement('ul');
+  document.querySelector('#taglistIngredients');
+  for (const element of ingredients) {
+    const list = document.createElement('li');
+    zoneList.appendChild(list);
+    list.innerText = element;
+    console.log(element);
+  }
+  document.querySelector('#listIngredient').appendChild(zoneList);
 }
+
+// APPAREILS
+
+// USTENSILES
